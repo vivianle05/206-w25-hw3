f4fed47f39be3b955af79ddaff0de18a1499bdc6
vivianle05 <98407372+vivianle05@users.noreply.github.com>
2025-01-26 14:19:20 -0500

    Add files via upload

---
+++ hw03_starter.py
@@ -0,0 +1,50 @@
+# Your name:
+# Your student id:
+# Your email:
+# List of who or what you worked with on this homework:
+# If you used Generative AI, say that you used it and also how you used it.
+
+
+## Import any necessary modules
+
+
+## Define class
+
+    ## define the __init__ method
+    # def __init__(self, answers):
+    #     pass
+        
+
+    ## define the __str__ method
+    # def __str__(self):
+    #    pass
+
+    ## define get_fortune method
+    
+
+    ## define play_game method
+    
+
+    ## define print_answer_frequencies method
+    
+
+# Main function
+def main():
+    # 1. Define the possible answers into a list: 
+
+    # 2. Create a MagicEightBall object
+
+    # 3. Print the MagicEightBall object 
+
+    # 4. Initiate the game play using the play_game() method
+
+    # 5. Call print_answer_frequencies()
+
+    # 6. Print the MagicEightBall object 
+
+
+
+# Run the main function if the file is executed directly
+if __name__ == "__main__":
+    main()
+   
